1f3365c4b18a1e0429c912ede447ad5413a06e79
mikey1977 <mmi@umich.edu>
2015-09-02 16:57:26 -1000

    secret-number is repeating

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,10 @@
 'use strict';
 module.exports = function() {
-  
+  var module = {};
+  var secretNumber = (Math.random() * 1000000);
+  return function () {
+    return secretNumber;
+  };
+
 };
+console.log( module.exports());
